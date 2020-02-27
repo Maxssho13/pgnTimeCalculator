14ff020a7804d54ade99220146223d30ea7435a5
Maxssho13 <glassmax13@gmail.com>
2020-02-26 21:17:24 -0500

    complete rework of everything
    Signed-off-by: Maxssho13 <glassmax13@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-import math
 import sys
 import os
+import re
+from itertools import chain
+
 
 class Game:
     def __init__(self, player1="", player2="", winner=0, startTime=0, increment=0, totalWhite=0, totalTime=0, totalBlack=0):
@@ -14,63 +16,98 @@ class Game:
         self.totalBlack = totalBlack
         self.totalTime = totalTime
 
+
 def main():
-    games = []
+
+    games[i] = Game()
     # all games in specified folder
     dirs = os.listdir(sys.argv[1])
     dirs.remove(".DS_Store")
 
-    for i in range(len(dirs)):
-        games.append(Game())
+    dirname = os.path.dirname(__file__)
+    # grabs file index 'i'
+    fPath = os.path.join(dirname, sys.argv[1]+"/"+dirs[i])  # <--[i]
 
-        dirname = os.path.dirname(__file__)
-        fn = os.path.join(dirname, sys.argv[1]+"/"+dirs[i])
+    file = open(fPath, "r")
 
-        file = open(fn,"r")
+    wholeFile = file.read()
 
-        f1 = file.readlines()
+    # find result of game
+    result = re.findall("Result .*", wholeFile)
+    if len(result[0]) > 13:
+            games[i].winner = 0.5
+        else:
+            # 1 = white, 0 = black, 0.5 = draw
+            games[i].winner = result[0][8]
+
+        # go back to top of file
+        file.seek(0)
+        fileLines = file.readlines()
+
+        moveTimes = []
+        with open(fPath) as input_data:
+            # Skips text before the beginning of the interesting block:
+            for line in input_data:
+                if line.strip() == '':  # Or whatever test is needed
+                    break
+        # Reads text until the end of the block:
+            for line in input_data:  # This keeps reading the file
+                # print(line)
+                moveTimes.append(re.findall(
+                    "[0-9]?[0-9]:[0-9][0-9]:[0-9][0-9].?[0-9]?", line))
+            # change 2d list to 1d
+            moveTimes = list(chain.from_iterable(moveTimes))
 
         # grab players
-        if "Maxssho13" in f1[4]:
+        if "Maxssho13" in fileLines[4]:
             white = "Maxssho13"
-            black = f1[5][8:len(f1[5])-3]
+            black = fileLines[5][8:len(fileLines[5])-3]
         else:
-            white = f1[4][8:len(f1[4])-3]
+            white = fileLines[4][8:len(fileLines[4])-3]
             black = "Maxssho13"
-
 
         games[i].player1 = white
         games[i].player2 = black
-        # winner: 1 = black win, 0 = white win
-        games[i].winner = int(f1[6][9:len(f1[6])-5])
 
-        splitTime = f1[10].split("+")
-        games[i].startTime = int(splitTime[0][14:len(splitTime[0])])
-        games[i].increment = int(splitTime[1][0:len(splitTime[1])-3])
+        # moveTimes = re.findall(
+        # "[0-9]?[0-9]:[0-9][0-9]:[0-9][0-9].?[0-9]?", wholeFile)
 
+        timeControl = re.findall("TimeControl .*", wholeFile)
+        times = [string for string in re.findall(
+            "[0-9]*", timeControl[0]) if string != ""]
 
-        lastLineTimes = f1[len(f1)-1].split("%clk ")
+        # find start time and increment
+        games[i].startTime = int(times[0])
+        games[i].increment = int(times[1])
 
+        print("white: "+games[i].player1)
+        print("black: "+games[i].player2)
+        print("start time: "+str(games[i].startTime))
+        print("increment: "+str(games[i].increment))
+        print("result: "+str(games[i].winner))
 
-        if len(lastLineTimes) > 2:
-        # if both end times are on last line
-            if f1[len(f1)-1][3] == ".":
-                games[i].totalWhite = games[i].startTime - (int(lastLineTimes[2][0])*3600+int(lastLineTimes[2][2:4])*60+float(lastLineTimes[2][5:9]))
-                games[i].totalBlack = games[i].startTime - (int(lastLineTimes[1][0])*3600+int(lastLineTimes[1][2:4])*60+float(lastLineTimes[1][5:9]))
-            else:
-                games[i].totalWhite = games[i].startTime - (int(lastLineTimes[1][0])*3600+int(lastLineTimes[1][2:4])*60+float(lastLineTimes[1][5:9]))
-                games[i].totalBlack = games[i].startTime - (int(lastLineTimes[2][0])*3600+int(lastLineTimes[2][2:4])*60+float(lastLineTimes[2][5:9]))
-        # 1 end time on last line. 1 on 2nd to last
-        else:
-            print(f1[len(f1)-2])
+        if len(moveTimes) % 2 == 1:
+            # black played last move
+            lastMoveWhiteSecond = (int(moveTimes[-1][0]) * 60 * 60) + (int(moveTimes[-1][2])*600) + (
+                int(moveTimes[-1][3])*60) + (int(moveTimes[-1][5])*10) + (int(moveTimes[-1][6]))
+            lastMoveBlackSecond = (int(moveTimes[len(moveTimes)-2][0]) * 60 * 60) + (int(moveTimes[len(moveTimes)-2][2]) * 600) + (
+                int(moveTimes[len(moveTimes)-2][3])*60)+(int(moveTimes[len(moveTimes)-2][5])*10) + (int(moveTimes[len(moveTimes)-2][6]))
+        if len(moveTimes) % 2 == 0:
+            # white played last move
+            lastMoveBlackSecond = (int(moveTimes[-1][0]) * 60 * 60) + (int(moveTimes[-1][2])*600) + (
+                int(moveTimes[-1][3])*60) + (int(moveTimes[-1][5])*10) + (int(moveTimes[-1][6]))
+            lastMoveWhiteSecond = (int(moveTimes[len(moveTimes)-2][0]) * 60 * 60) + (int(moveTimes[len(moveTimes)-2][2]) * 600) + (
+                int(moveTimes[len(moveTimes)-2][3])*60)+(int(moveTimes[len(moveTimes)-2][5])*10) + (int(moveTimes[len(moveTimes)-2][6]))
 
+        games[i].totalWhite = games[i].startTime - \
+            lastMoveWhiteSecond + len(moveTimes)/2*games[i].increment
+        games[i].totalBlack = games[i].startTime - \
+            lastMoveBlackSecond + len(moveTimes)/2*games[i].increment
 
-
-
+        print("white time: "+str(games[i].totalWhite))
+        print("black time: "+str(games[i].totalBlack))
         games[i].totalTime = games[i].totalBlack + games[i].totalWhite
-        games[i].totalTime += int(f1[len(f1)-1].split(".")[0]) * 2 * games[i].increment
+        print("total time: "+str(games[i].totalTime))
 
-        print(math.floor(games[i].totalTime/60), round(games[i].totalTime%60*10)/10)
-        file.close()
 
 main()
